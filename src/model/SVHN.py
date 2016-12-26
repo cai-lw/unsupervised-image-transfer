46e36aa380aea28bcb3db50753ccf4993a3ade29
cai-lw <cai_lw@126.com>
2016-12-26 16:03:02 +0800

    Revert SVHN back ...

--- src/model/SVHN.py
+++ src/model/SVHN.py
@@ -29,41 +29,38 @@ class SVHN(object):
         self.checkpoint_dir = checkpoint_dir
         self.build_model()
 
-    def net(self, images, reuse=False):
-        if reuse:
-            tf.get_variable_scope().reuse_variables()
+    def net(self, images, reuse=None):
+        with tf.variable_scope("net", reuse=reuse):
+            # 32 * 32 * 3
+            h0_conv = relu(conv2d(images, 16, k_h=3, k_w=3, d_h=1, d_w=1, name='h0_conv'))
+            h1_conv = relu(conv2d(h0_conv, 16, k_h=3, k_w=3, d_h=1, d_w=1, name='h1_conv'))
+            h1_pool = maxpooling2d(h1_conv, k_h=2, k_w=2, step_h=2, step_w=2)
 
-        # 32 * 32 * 3
-        h0_conv = relu(conv2d(images, 16, k_h=3, k_w=3, d_h=1, d_w=1, name='h0_conv'))
-        h1_conv = relu(conv2d(h0_conv, 16, k_h=3, k_w=3, d_h=1, d_w=1, name='h1_conv'))
-        h1_pool = maxpooling2d(h1_conv, k_h=2, k_w=2, step_h=2, step_w=2)
+            # 16 * 16 * 16
+            h2_conv = relu(conv2d(h1_pool, 32, k_h=3, k_w=3, d_h=1, d_w=1, name='h2_conv'))
+            h3_conv = relu(conv2d(h2_conv, 32, k_h=3, k_w=3, d_h=1, d_w=1, name='h3_conv'))
+            h3_pool = maxpooling2d(h3_conv, k_h=2, k_w=2, step_h=2, step_w=2)
 
-        # 16 * 16 * 16
-        h2_conv = relu(conv2d(h1_pool, 32, k_h=3, k_w=3, d_h=1, d_w=1, name='h2_conv'))
-        h3_conv = relu(conv2d(h2_conv, 32, k_h=3, k_w=3, d_h=1, d_w=1, name='h3_conv'))
-        h3_pool = maxpooling2d(h3_conv, k_h=2, k_w=2, step_h=2, step_w=2)
+            # 8 * 8 * 32
+            h4_conv = relu(conv2d(h1_pool, 64, k_h=3, k_w=3, d_h=1, d_w=1, name='h4_conv'))
+            h5_conv = relu(conv2d(h2_conv, 64, k_h=3, k_w=3, d_h=1, d_w=1, name='h5_conv'))
+            h5_pool = maxpooling2d(h3_conv, k_h=2, k_w=2, step_h=2, step_w=2)
 
-        # 8 * 8 * 32
-        h4_conv = relu(conv2d(h1_pool, 64, k_h=3, k_w=3, d_h=1, d_w=1, name='h4_conv'))
-        h5_conv = relu(conv2d(h2_conv, 64, k_h=3, k_w=3, d_h=1, d_w=1, name='h5_conv'))
-        h5_pool = maxpooling2d(h3_conv, k_h=2, k_w=2, step_h=2, step_w=2)
+            # 4 * 4 * 64
+            h6_conv = relu(conv2d(h1_pool, 128, k_h=3, k_w=3, d_h=1, d_w=1, name='h6_conv'))
+            h7_conv = relu(conv2d(h2_conv, 128, k_h=3, k_w=3, d_h=1, d_w=1, name='h7_conv'))
+            h7_pool = maxpooling2d(h3_conv, k_h=2, k_w=2, step_h=2, step_w=2)
 
-        # 4 * 4 * 64
-        h6_conv = relu(conv2d(h1_pool, 128, k_h=3, k_w=3, d_h=1, d_w=1, name='h6_conv'))
-        h7_conv = relu(conv2d(h2_conv, 128, k_h=3, k_w=3, d_h=1, d_w=1, name='h7_conv'))
-        h7_pool = maxpooling2d(h3_conv, k_h=2, k_w=2, step_h=2, step_w=2)
+            #  2 * 2 * 128
+            h8_conv = relu(conv2d(h1_pool, 256, k_h=3, k_w=3, d_h=1, d_w=1, name='h8_conv'))
+            h9_conv = relu(conv2d(h2_conv, 256, k_h=3, k_w=3, d_h=1, d_w=1, name='h9_conv'))
+            h9_pool = maxpooling2d(h3_conv, k_h=2, k_w=2, step_h=2, step_w=2)
 
-        #  2 * 2 * 128
-        h8_conv = relu(conv2d(h1_pool, 256, k_h=3, k_w=3, d_h=1, d_w=1, name='h8_conv'))
-        h9_conv = relu(conv2d(h2_conv, 256, k_h=3, k_w=3, d_h=1, d_w=1, name='h9_conv'))
-        h9_pool = maxpooling2d(h3_conv, k_h=2, k_w=2, step_h=2, step_w=2)
+            # linear ops
+            h10_lin = linear(tf.reshape(h9_pool, [self.batch_size, -1]), 256, 'h10_lin')
+            res = linear(h10_lin, 10, 'res')
 
-        # linear ops
-        h10_lin = linear(tf.reshape(h9_pool, [self.batch_size, -1]), 256, 'h10_lin')
-        res = linear(h10_lin, 10, 'res')
-
-        return res, h10_lin
-
+            return res, h10_lin
 
     def build_model(self):
 
@@ -85,11 +82,11 @@ class SVHN(object):
     def train(self, config):
         """Train SVHN Model"""
 
-        data_X_extra, data_y_extra = load_image_from_mat(os.path.join(config.src_dir, 'extra_32x32.mat'))
+        # data_X_extra, data_y_extra = load_image_from_mat(os.path.join(config.src_dir, 'extra_32x32.mat'))
         data_X_train, data_y_train = load_image_from_mat(os.path.join(config.src_dir, 'train_32x32.mat'))
         data_X_test, data_y_test = load_image_from_mat(os.path.join(config.src_dir, 'test_32x32.mat'))
-        data_X_train = np.concatenate((data_X_train, data_X_extra), axis=0)
-        data_y_train = np.concatenate((data_y_train, data_y_extra), axis=0)
+        # data_X_train = np.concatenate((data_X_train, data_X_extra), axis=0)
+        # data_y_train = np.concatenate((data_y_train, data_y_extra), axis=0)
         indices = np.arange(len(data_X_train))
         np.random.shuffle(indices)
         data_X_train = data_X_train[indices]
